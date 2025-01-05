eaa6de22b993f7b8b4d7636b11c1fa1a9fb95e4c
PankajSubedi <beingking90@gmail.com>
2025-01-05 17:18:11 +0545

    implement clerk webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
-import { clerkMiddleware } from '@clerk/nextjs/server'
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware()
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
@@ -8,6 +8,8 @@ export const config = {
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
-  ],
-}
 
+    // Exclude specific public routes (e.g., webhooks)
+    '/api/webhooks/clerk',
+  ],
+};
